767513eba206c600438900909e9fe1e47c7b7296
Anurag Jain <anuragj4711@outlook.com>
2025-03-10 21:00:13 +0530

    1008 Div 2

---
+++ final_verdict.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr double PI = 3.141592653589793;
+constexpr int MOD = 1000000007;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define usl unordered_set<ll>
+#define umll unordered_map<ll, ll>
+#define umsi unordered_map<string, int>
+
+#define fi first
+#define se second
+#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
+#define RFOR(i, a, b) for (ll i = (ll)a; i > (ll)b; i--)
+#define all(v) (v).begin(), (v).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve()
+{
+    int n, x, sum = 0;
+    cin >> n >> x;
+    
+    FOR(i, 0, n)
+    {
+        int k;
+        cin >> k;
+        sum += k;
+    }
+
+    string res = (sum / n == x) ? "YES" : "NO";
+    cout << res << endl;
+}
+
+int main()
+{
+    FAST_IO
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
