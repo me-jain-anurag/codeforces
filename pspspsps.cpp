c3a9b2bac99b786714523596c530e8eca45677ea
Anurag Jain <anuragj4711@outlook.com>
2025-03-12 19:52:52 +0530

    994 Div 2

--- pspspsps.cpp
+++ pspspsps.cpp
@@ -1,27 +1,6 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-constexpr double PI = 3.141592653589793;
-constexpr int MOD = 1000000007;
-
-#define ll long long
-#define ld long double
-#define ull unsigned long long
-#define vll vector<long long>
-#define vvll vector<vector<long long>>
-#define umii unordered_map<int, int>
-#define umll unordered_map<ll, ll>
-#define unsi unordered_map<string, int>
-
-#define $ "\n"
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
-#define RFOR(i, a, b) for (ll i = (ll)a; i >= (ll)b; i--)
-#define all(v) (v).begin(), (v).end()
-#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
 
 void solve()
 {
@@ -29,62 +8,47 @@ void solve()
     cin >> n;
     string s;
     cin >> s;
-
-    bool res = true;
-    bool flag = false;
-    FOR(i, 0, n)
+    for (int i = 0; i < n; i++)
     {
-        if (flag)
+        if (s[i] == 'p')
         {
-            if (s[i] == 's') res = false;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (s[j] == 's')
+                {
+                    cout << "NO\n";
+                    return;
+                }
+            }
             break;
         }
-        else if (s[i] == 'p') flag = true;
-    }
-
-    flag = false;
-    int cnt = 0, pcnt = 0;
-    FOR(i, 0, n)
-    {
-        if (flag)
+        if (s[i] == 's' && i != 0)
         {
-            if (s[i] == 's') cnt++;
-            if (s[i] == 'p')
+            for (int j = i + 1; j < n; j++)
             {
-                if (pcnt > 0)
+                if (s[j] == 'p' && j < n - 1)
                 {
-                    res = false;
-                    break;
+                    cout << "NO\n";
+                    return;
                 }
-                if (cnt == 0)
-                {
-                    if (s[0] == 's' || i == (n - 1))
-                    {
-                        pcnt++;
-                        continue;
-                    }
-                    res = false;
-                    break;
-                }
-                else if (i == n - 1) continue;
-                res = false;
-                break;
             }
+            break;
         }
-        else if (s[i] == 's') flag = true;
     }
-
-    if (res) cout << "YES\n";
-    else cout << "NO\n";
+    cout << "YES\n";
+    return;
 }
 
 int main()
 {
-    FAST_IO
-
-    int t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
     cin >> t;
-    while (t--) { solve(); }
+    while (t--)
+    {
+        solve();
+    }
 
     return 0;
 }
