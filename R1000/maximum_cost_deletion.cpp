7d8e9baed1761ad9aab10dec99c95504eab9458b
Anurag Jain <anuragj4711@outlook.com>
2025-03-14 18:48:42 +0530

    Educational Codeforces Round 111

---
+++ R1000/maximum_cost_deletion.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr double PI = 3.141592653589793;
+constexpr int MOD = 1000000007;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define usl unordered_set<ll>
+#define umll unordered_map<ll, ll>
+#define umsi unordered_map<string, int>
+
+#define fi first
+#define se second
+#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
+#define RFOR(i, a, b) for (ll i = (ll)a; i > (ll)b; i--)
+#define all(v) (v).begin(), (v).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    string s;
+    cin >> s;
+
+    if (b >= 0)
+    {
+        cout << (a + b) * n << endl;
+        return;
+    }
+
+    int cnt = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] != s[i - 1]) cnt++;
+    }
+
+    cout << (n * a) + (b * ((cnt + 3) / 2)) << endl;
+}
+
+int main()
+{
+    FAST_IO
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
