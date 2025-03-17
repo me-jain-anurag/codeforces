8355c6c202955ffedbf0911a3e6a923703c1ed70
Anurag Jain <anuragj4711@outlook.com>
2025-03-17 21:23:13 +0530

    Educational Round 111

---
+++ R800/find_the_array.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr double PI = 3.141592653589793;
+constexpr int MOD = 1000000007;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define usl unordered_set<ll>
+#define umll unordered_map<ll, ll>
+#define umsi unordered_map<string, int>
+
+#define fi first
+#define se second
+#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
+#define RFOR(i, a, b) for (ll i = (ll)a; i > (ll)b; i--)
+#define all(v) (v).begin(), (v).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << (int) sqrt(n - 1) + 1 << endl;
+}
+
+int main()
+{
+    FAST_IO
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
