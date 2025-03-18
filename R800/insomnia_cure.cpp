4d6a1cbeb71ab1884c3eceb4d7be2be11bea8804
Anurag Jain <anuragj4711@outlook.com>
2025-03-18 17:26:32 +0530

    Round 105 Div 2

---
+++ R800/insomnia_cure.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr double PI = 3.141592653589793;
+constexpr int MOD = 1000000007;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define usl unordered_set<ll>
+#define umll unordered_map<ll, ll>
+#define umsi unordered_map<string, int>
+
+#define fi first
+#define se second
+#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
+#define RFOR(i, a, b) for (ll i = (ll)a; i > (ll)b; i--)
+#define all(v) (v).begin(), (v).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve()
+{
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    int ans = 0;
+
+    for (int i = 1; i <= d; i++)
+    {
+        if ((i % k == 0) || (i % l == 0) || (i % m == 0) || (i % n == 0)) ans++;
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    FAST_IO
+    int t; t = 1; while (t--) solve();
+    return 0;
+}
