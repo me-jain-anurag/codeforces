1a5b8f694708810811c03166d34675173444292b
Anurag Jain <anuragj4711@outlook.com>
2025-06-01 17:04:05 +0530

    1102 A

---
+++ R800/integer_sequence_dividing.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr double PI = 3.141592653589793;
+constexpr int MOD = 1000000007;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define usl unordered_set<ll>
+#define umll unordered_map<ll, ll>
+#define umsi unordered_map<string, int>
+
+#define fi first
+#define se second
+#define FOR(i, a, b) for (ll i = (ll)a; i < (ll)b; i++)
+#define RFOR(i, a, b) for (ll i = (ll)a; i > (ll)b; i--)
+#define all(v) (v).begin(), (v).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    n = n % 4;
+    if (n == 0 or n == 3) cout << 0;
+    else cout << 1;
+}
+
+int main()
+{
+    FAST_IO
+    int t; t = 1; while (t--) solve();
+    return 0;
+}
