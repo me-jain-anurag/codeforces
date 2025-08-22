4f22b28fad712f2e792f5493560ffddd8fd398e3
me-jain-anurag <132147016+me-jain-anurag@users.noreply.github.com>
2025-08-22 19:30:52 +0530

    Correct the solution
    Complete and correct the hard verion of the cunning seller

--- the_cunning_seller_hard_version.cpp
+++ the_cunning_seller_hard_version.cpp
@@ -12,50 +12,44 @@ using namespace std;
 #define pb push_back
 #define endl '\n'
 
-vector<ll> a(23), g(24);
-
-for (int i = 0; i < 23; i++) {
-    a[i] = pow(3, i + 1) + i * pow(3, i - 1);
-    g[i] = pow(3, i);
-}
-
-g[23] = pow(3, 23);
-
-bool isPossible(ll p, ll n, ll prev, int i, ll mid) {
-    ll o = n - (mid * g[i]) - prev;
-    if (mid * g[i] + o * g[i - 1] <= p - prev) return true;
-    else return false;
-}
-
 void solve() {
-    ll n, p;
-    cin >> n >> p;    
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> a(23);
+
+    for (int i = 0; i < 23; i++) {
+        a[i] = pow(3, i + 1) + i * pow(3, i - 1);
+    }
+
+    string num;
+    ll x = n;
+    int p = 0;
+
+    while (x > 0) {
+        p += x % 3;
+        num += (char) ((x % 3) + '0');
+        x /= 3;
+    }
+
+    if (p > k) {
+        cout << -1 << endl;
+        return;
+    }
 
     ll res = 0LL;
-    prev = 0;
-    for (int i = 0; i < 23; i++) {
-        ll k = n / g[i];
-        if (k + prev > p) {
-            ll j = ((n % g[i + 1]) - prev);
-            res += j * a[i];
-            prev = (n % g[i + 1]);
-            continue;
+    int s = num.size();
+
+    for (int i = s - 1; i > 0; i--) {
+        while (p + 2 <= k && num[i] > '0') {
+            num[i]--;
+            num[i - 1] += 3;
+            p += 2;
         }
-        if (i == 0) {
-            cout << 3 * n << endl;
-            return;
-        }
-        ll low = 1, high = k, ans = -1;
-        while (low <= high) {
-            ll mid = low + (high - low) / 2;
-            if (isPossible(p, n, prev, i, mid)) {
-                ans = mid;
-                high = mid - 1;
-            }
-            else low = mid + 1;
-        }
-        res += ans * a[i] + ((n - ans - prev) / g[i - 1]) * a[i - 1];
-        break;
+    }
+
+    for (int i = 0; i < s; i++) {
+        res += (num[i] - '0') * a[i];
     }
 
     cout << res << endl;
